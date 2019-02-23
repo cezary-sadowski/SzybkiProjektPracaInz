7da26738f1d8655a65811d6380130a5b7402992f
Cezary Sadowski <cezary.sadowski@aplixcom.com>
2019-02-23 23:18:11 +0100

    Aktywne zlecenie + liczenie wskaznika OEE

---
+++ pracainz/Controllers/ZlecenieController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pracainz.Models;
+
+namespace pracainz.Controllers
+{
+    public class ZlecenieController : Controller
+    {
+        private ERP_DB ctx;
+
+        public ZlecenieController()
+        {
+            ctx = new ERP_DB();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            ctx.Dispose();
+        }
+
+        // GET: Zlecenie
+        public ViewResult Index()
+        {
+            var tasks = GetTasks();
+            foreach (var t in tasks)
+            {
+                t.OEE = CountOEE(t.Dostepnosc, t.Wydajnosc, t.Jakosc);
+            }
+            ctx.SaveChanges();
+
+            return View(tasks);
+        }
+
+        private IEnumerable<Zlecenie> GetTasks()
+        {
+            return ctx.Zlecenie.ToList();
+        }
+
+        private double? CountOEE(double? dostepnosc, double? wydajnosc, double? jakosc)
+        {
+            return (dostepnosc * wydajnosc * jakosc) / 1000 ;
+        }
+    }
+}
