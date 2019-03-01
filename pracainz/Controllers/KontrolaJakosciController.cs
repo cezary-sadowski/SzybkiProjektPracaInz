ba54e1737119c81a2bce67355ea9bb6f06c92541
Cezary Sadowski <cez94k@gmail.com>
2019-03-01 17:47:31 +0100

    Technolog i Kontrola Jakosci dodanie controller i view

---
+++ pracainz/Controllers/KontrolaJakosciController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pracainz.Models;
+
+namespace pracainz.Controllers
+{
+    public class KontrolaJakosciController : Controller
+    {
+        private ERP_DB ctx;
+
+        public KontrolaJakosciController()
+        {
+            ctx = new ERP_DB();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            ctx.Dispose();
+        }
+
+        // GET: KontrolaJakosci
+        public ViewResult Index()
+        {
+            var kj = GetKJ();
+
+            return View(kj);
+        }
+
+        private IEnumerable<KontrolaJakosci> GetKJ() => ctx.KontrolaJakosci.ToList();
+    }
+}
---
+++ pracainz/Controllers/TechnologController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pracainz.Models;
+
+namespace pracainz.Controllers
+{
+    public class TechnologController : Controller
+    {
+        private ERP_DB ctx;
+
+        public TechnologController()
+        {
+            ctx = new ERP_DB();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            ctx.Dispose();
+        }
+
+        // GET: Technolog
+        public ViewResult Index()
+        {
+            var kj = GetTechnolog();
+
+            return View(kj);
+        }
+
+        private IEnumerable<Technolog> GetTechnolog() => ctx.Technolog.ToList();
+    }
+}
